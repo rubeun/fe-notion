37d0c85a9de4e4114ed5986d2b211b9db35f41da
Rubeun <rubeun@Rubeuns-MacBook-Pro-2019.local>
2023-09-08 01:00:10 -0700

    add initialUnivDataLoad function to load csv file as a string

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 
+const UNI_DATA = 'https://gist.githubusercontent.com/simonlast/d5a86ba0c82e1b0d9f6e3d2581b95755/raw/f608b9b896dd3339df13dae317998d5f24c00a50/edu-scorecard.csv';
+
 const App = () => {
+  const [uniData, setUniData] = useState([]);
+
+  const initialUnivDataLoad = () => {
+    try {
+      fetch(UNI_DATA)
+        .then(response => response.text())
+        .then(responseText => console.log(responseText));
+    } catch (error) {
+      console.error("CSV Data failed to load", error);
+    }
+  }
+  initialUnivDataLoad();
   return (
     <div className="App">
     </div>
